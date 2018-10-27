2c7f27acc159e05be52939c20b25494000b73409
Regan Dufort <regan.t.dufort@gmail.com>
2018-10-27 13:27:39 -0700

    Question 3 actually 4 on array

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ function sumAndMultiply(a, b, c) {
   var multiplyABC = multiply(multiplyAB, c)[0];
   console.log('ab times c', multiplyABC);
 
-  var sentenceMultiply = 'The product of '+ a + ' and ' + b + ' and ' + c + ' is ' + multiplyABC + '.';
-console.log(sentenceMultiply);
+  var sentenceMultiply = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiplyABC + '.';
+  console.log(sentenceMultiply);
 
   return [aPlusBplusC, multiplyABC, sentenceAdd, sentenceMultiply];
 }
@@ -50,8 +50,43 @@ console.log(sentenceMultiply);
 testSumAndMultiply(4, 7, 5);
 
 
-// var sumAndMultiply = sumAndMultiply(aplusb,AplusBplusC);
-// console.log(sumAndMultiply);
+/* Problem 4
+Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+"2,3,4 was passed in as an array of numbers, and 9 is their sum."
+
+IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
+
+// Write your code here
+var testArray = [2, 3, 4]; //eslint-disable-line
+sumArray(testArray);
+
+
+
+function sumArray(sumArr) { //eslint-disable-line
+
+  var sumTotal = 0;
+  console.log('Total: ' + sumTotal);
+
+  for (var i = 0; i < sumArr.length; i++) {
+    sumTotal = sum(sumTotal, sumArr[i])[0];
+    console.log('Total: ' + sumTotal);
+  }
+
+
+  var arrayAsString = sumArr.join();
+
+  var sentence = arrayAsString + ' was passed in as an array of numbers, and ' + sumTotal + ' is their sum.'; 
+  console.log('sentence of sum array: '+ sentence);
+
+  return[sumTotal, sentence];
+}
+
+
+// Here is the test for sumArray(); uncomment it to run it
+
+ testSumArray(testArray);
+
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
