b7503b73f7179897964701a7fea13d86d85ed280
Regan Dufort <regan.t.dufort@gmail.com>
2018-10-27 14:34:51 -0700

    Question 4 multiply arrays

--- app.js
+++ app.js
@@ -78,15 +78,59 @@ function sumArray(sumArr) { //eslint-disable-line
 
   var arrayAsString = sumArr.join();
 
-  var sentence = arrayAsString + ' was passed in as an array of numbers, and ' + sumTotal + ' is their sum.'; 
-  console.log('sentence of sum array: '+ sentence);
+  var sentence = arrayAsString + ' was passed in as an array of numbers, and ' + sumTotal + ' is their sum.';
+  console.log('sentence of sum array: ' + sentence);
 
-  return[sumTotal, sentence];
+  return [sumTotal, sentence];
 }
 
 
 // Here is the test for sumArray(); uncomment it to run it
 
- testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+
+
+/////////////////////////////////////
+/* Problem 5
+Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+
+"The numbers 2,3,4 have a product of 24."
+
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements.
+
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
+
+// Write your code here
+
+
+function multiplyArray(multArr) { //eslint-disable-line
+
+  var multiplyTotal = 1;
+  console.log('Total: ', multiplyTotal);
+  for(var i = 0; i < multArr.length; i++){
+    multiplyTotal = multiply(multiplyTotal, multArr[i])[0];
+    console.log('multiply total: ', + multiplyTotal);
+
+  }
+
+  var sentenceAsString = multArr.join();
+  console.log('sentence as a string: ', sentenceAsString);
+  var sentenceMultiplyString = 'The numbers ' + sentenceAsString + ' have a product of ' + multiplyTotal + '.';
+
+  return [multiplyTotal, sentenceMultiplyString];
+
+}
+// multiplyArray(testArray);
+
+// Here is the test for multiplyArray(); uncomment it to run it
+testMultiplyArray(testArray);
+
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+
+// You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
+
+// Don't forget to create a new branch for your work on the next question, if you attempt it.
+
+/////////////////////////////////////
